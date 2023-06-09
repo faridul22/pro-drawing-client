b7d3114657249e863c782af357d6599abcdc6807
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-09 21:54:31 +0600

    initial project setup done with primary navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+    </>
+  )
+}
+
+export default App
