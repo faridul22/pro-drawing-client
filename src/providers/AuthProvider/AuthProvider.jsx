e1e783fb05721ebfc8c6fe3bd5bd5319a041f64a
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-12 23:57:37 +0600

    jwt token store and remove in localStorage

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './../../firebase/firebase.config';
+import axios from "axios";
 // import axios from "axios";
 
 const auth = getAuth(app);
@@ -43,17 +44,17 @@ const AuthProvider = ({ children }) => {
             console.log('current user', currentUser)
 
             // get and set token
-            // if (currentUser) {
-            //     axios.post('http://localhost:5000/jwt', { email: currentUser.email })
-            //         .then(data => {
-            //             console.log(data.data.token)
-            //             localStorage.setItem('access-token', data.data.token)
-            setLoading(false)
-            //         })
-            // }
-            // else {
-            //     localStorage.removeItem('access-token')
-            // }
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token)
+                        localStorage.setItem('access-token', data.data.token)
+                        setLoading(false)
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
 
 
         })
