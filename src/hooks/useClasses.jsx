ab97395cfc8e954cc6b6bbb2533d2d1d10feb18d
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-13 06:49:16 +0600

    create a custom hook by using axios instance

--- src/hooks/useClasses.jsx
+++ src/hooks/useClasses.jsx
@@ -4,12 +4,17 @@ import { useQuery } from "@tanstack/react-query";
 
 
 const useClasses = () => {
-    const { user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token')
 
     const { isLoading, refetch, data: selectedClasses = [], } = useQuery({
         queryKey: ['selectedclasses', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/selectedclasses?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/selectedclasses?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
 
             return response.json()
         },
