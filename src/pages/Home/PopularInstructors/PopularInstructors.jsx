7bc1a01e339e1d70ba7d400a7c35a3847430e078
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-16 02:07:46 +0600

    popular instructors data changed

--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,38 +1,26 @@
+import { useEffect, useState } from "react";
 import InstructorsCard from "../../../components/ClassesCard/InstructorsCard";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import useAllClasses from "../../../hooks/useAllClasses";
 
 
 const PopularInstructors = () => {
-    const [allClasses] = useAllClasses();
-    const i = []
-    console.log(i)
+    const [instructors, setInstructors] = useState([]);
+
+    useEffect(() => {
+        fetch('https://pro-drawing-server.vercel.app/instructors')
+            .then(res => res.json())
+            .then(data => setInstructors(data))
+    }, [])
     return (
         <div className="mt-20">
             <SectionTitle subHeading="Our most" heading="Popular Instructors"></SectionTitle>
             <div className="grid lg:grid-cols-3 gap-6 mt-10">
                 {
+                    instructors.slice(0, 6).map(instructor => <InstructorsCard
+                        key={instructor._id}
+                        instructor={instructor}
+                    ></InstructorsCard>)
 
-                    allClasses?.slice(0, 6).map(instructor => {
-                        if (i.length == 0) {
-                            i.push(instructor.instructorEmail)
-                            return <InstructorsCard
-                                key={instructor._id}
-                                instructor={instructor}
-                            ></InstructorsCard>
-                        }
-
-                        if (i.indexOf(instructor.instructorEmail) == -1) {
-                            i.push(instructor.instructorEmail)
-                            return <InstructorsCard
-                                key={instructor._id}
-                                instructor={instructor}
-                            ></InstructorsCard>
-
-                        }
-
-
-                    })
                 }
             </div>
         </div>
