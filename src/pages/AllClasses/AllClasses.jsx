73c5dc44ba59971130ea999121f48f4478072d04
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-15 21:49:19 +0600

    show approve in classes route

--- src/pages/AllClasses/AllClasses.jsx
+++ src/pages/AllClasses/AllClasses.jsx
@@ -6,7 +6,7 @@ const AllClasses = () => {
     const [classes, setClasses] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/classes')
+        fetch('http://localhost:5000/approvedclasses')
             .then(res => res.json())
             .then(data => setClasses(data))
     }, [])
--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -5,15 +5,34 @@ import useAllClasses from "../../../hooks/useAllClasses";
 
 const PopularInstructors = () => {
     const [allClasses] = useAllClasses();
+    const i = []
+    console.log(i)
     return (
         <div className="mt-20">
             <SectionTitle subHeading="Our most" heading="Popular Instructors"></SectionTitle>
             <div className="grid lg:grid-cols-3 gap-6 mt-10">
                 {
-                    allClasses?.slice(0, 6).map(instructor => <InstructorsCard
-                        key={instructor._id}
-                        instructor={instructor}
-                    ></InstructorsCard>)
+
+                    allClasses?.slice(0, 6).map(instructor => {
+                        if (i.length == 0) {
+                            i.push(instructor.instructorEmail)
+                            return <InstructorsCard
+                                key={instructor._id}
+                                instructor={instructor}
+                            ></InstructorsCard>
+                        }
+
+                        if (i.indexOf(instructor.instructorEmail) == -1) {
+                            i.push(instructor.instructorEmail)
+                            return <InstructorsCard
+                                key={instructor._id}
+                                instructor={instructor}
+                            ></InstructorsCard>
+
+                        }
+
+
+                    })
                 }
             </div>
         </div>
