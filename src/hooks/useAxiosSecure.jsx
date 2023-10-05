6ff8733bcef7abcbfa50ab0369db10a195f492b0
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-10-05 22:23:52 +0600

    baseURL change

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://pro-drawing-server-faridul22.vercel.app',
 });
 
 const useAxiosSecure = () => {
