3af963acbebf6e2662abf18b947b64c6ccfa9f82
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-01 22:20:53 +0600

    Light & Dark theme issue fixed

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import Footer from "../pages/Shared/Footer/Footer";
-import { useState } from "react";
 
 
 const Main = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
-    };
     return (
-        <div data-theme={isDarkMode && "dark"}>
-            <Navbar toggleDarkMode={toggleDarkMode}></Navbar>
+        <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
